949c673bed0ffa2f8c2bb4df3602c5fb9b2ded7e
ToTheHit <dmtshkv@gmail.com>
2020-05-20 23:27:45 +0300

    Updated GitHub Pages version to 0.1

---
+++ precache-manifest.e98d8c7eb9c805253337f45c6797d920.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de83916c489000956bc7e6cf7ea4a9a5",
+    "url": "/vk-extension-miniApp/index.html"
+  },
+  {
+    "revision": "2aee2eb51c67f315b9ed",
+    "url": "/vk-extension-miniApp/static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "52d687fefcec56a7313c",
+    "url": "/vk-extension-miniApp/static/css/main.fb3698ce.chunk.css"
+  },
+  {
+    "revision": "2aee2eb51c67f315b9ed",
+    "url": "/vk-extension-miniApp/static/js/2.bf95e2c8.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "/vk-extension-miniApp/static/js/2.bf95e2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d687fefcec56a7313c",
+    "url": "/vk-extension-miniApp/static/js/main.c866e64c.chunk.js"
+  },
+  {
+    "revision": "90be4943d7e65d5ba8db",
+    "url": "/vk-extension-miniApp/static/js/runtime-main.a9232dc7.js"
+  },
+  {
+    "revision": "71ed90b3285019275ff20a8b57e194d5",
+    "url": "/vk-extension-miniApp/static/media/intro-logo-dark.71ed90b3.png"
+  },
+  {
+    "revision": "7d8b4497028f0ea7097abf7ede196f5c",
+    "url": "/vk-extension-miniApp/static/media/intro-logo-light.7d8b4497.png"
+  },
+  {
+    "revision": "e2716b68611a1827acf3a37ff551ed14",
+    "url": "/vk-extension-miniApp/static/media/intro-market-dark_new.e2716b68.png"
+  },
+  {
+    "revision": "4fb1fc62687e3baf7156abcf9a2e9c56",
+    "url": "/vk-extension-miniApp/static/media/intro-market-light_new.4fb1fc62.png"
+  },
+  {
+    "revision": "b0356757453a50ec41ae0bd9f78e00f2",
+    "url": "/vk-extension-miniApp/static/media/intro-poll-dark.b0356757.png"
+  },
+  {
+    "revision": "1709f6a38e53235e077c2ad7a4e33830",
+    "url": "/vk-extension-miniApp/static/media/intro-poll-light.1709f6a3.png"
+  },
+  {
+    "revision": "20ad7a2af0ee1f803d72bb00d4965239",
+    "url": "/vk-extension-miniApp/static/media/opt_cherry-fail.20ad7a2a.png"
+  }
+]);
