2c71b9c240a992953ea4d0d39e9347cb7efaf313
ToTheHit <dmtshkv@gmail.com>
2020-05-20 23:24:05 +0300

    Updated GitHub Pages version to 0.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToTheHit/vk-extension-miniApp.git/precache-manifest.975c171bed92702f9400faa454883fde.js"
+  "/vk-extension-miniApp/precache-manifest.e98d8c7eb9c805253337f45c6797d920.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToTheHit/vk-extension-miniApp.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vk-extension-miniApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
