e00e420f8483ac1b14f3b0712254a1ff291f4937
ToTheHit <dmtshkv@gmail.com>
2020-05-20 22:18:28 +0300

    test

--- src/App.js
+++ src/App.js
@@ -66,7 +66,8 @@ const App = () => {
         <StartView id={globalVariables.view.start} nextView={setActiveView} />
         <CommonView id={globalVariables.view.main} nextView={setActiveView} />
         <WorkView id={globalVariables.view.work} nextView={setActiveView} />
-
+          <>
+            </>
         <TestView id={globalVariables.view.test} />
       </Root>
     </ConfigProvider>
